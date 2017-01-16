d997cb3382cdb9cb6de80b685f5d6431c88ee30f
Jailani Abdul Rahman <jailani.har@googlemail.com>
2017-01-16 10:20:38 +0800

    Implemented Methods, Constructors and Concept of Overloading

--- ch02/Car.java
+++ ch02/Car.java
@@ -8,17 +8,42 @@ public class Car {
 	int currentSpeed;
 	int totalMileage;
 	static int noOfCars;
+
+	public Car() {
+		this("Blue", 5, 0, 0, 0);
+	}
 	
-	public void pressAccelerator(int speed) {
+	public Car(int gasMileage, int currentSpeed, int totalMileage) {
+		this("Blue", 5, gasMileage, currentSpeed, totalMileage);
+	}
+	
+	public Car(String colour, int noOfDoors, int gasMileage, int currentSpeed, int totalMileage) {
+		this.colour = colour;
+		this.noOfDoors = noOfDoors;
+		this.gasMileage = gasMileage;
+		this.currentSpeed = currentSpeed;
+		this.totalMileage = totalMileage;
+		noOfCars++;
+	}
+
+	public String pressAccelerator(int speed) {
+		return pressAccelerator(speed, 5, 5);
+	}
+	
+	public String pressAccelerator(int speed, int gasConsumed, int totalTravelled) {
 		String msg = "Sudah ku takan";
-		System.out.println(msg);
 		currentSpeed += speed;
-		gasMileage -= 5;
-		totalMileage += 5;
+		gasMileage -= gasConsumed;
+		totalMileage += totalTravelled;
+		return msg;
 	}
 	
 	public void pressBrake() {
 		currentSpeed = 0;
+	}
+	
+	public static int getNoOfCars() {
+		return noOfCars;
 	}
 	
 }
